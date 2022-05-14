c7b8d23a16323eb80920faadb6308fd160fa03b9
Christian Algarin <christian.algarin002@xpo.com>
2022-05-14 20:56:50 -0300

    Test Component and Test Service added

---
+++ src/app/components/test-component/test-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  templateUrl: './test-component.component.html',
+  styleUrls: ['./test-component.component.scss']
+})
+export class TestComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("test component Init");
+  }
+
+}
